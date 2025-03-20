9edb70580311d60d4305948193df296286c2df77
martin <martin.ferk1@student.um.si>
2025-03-20 23:06:11 +0100

    Zacetek funkcije za obdelavo slik

--- OpenCVnaloga1.py
+++ OpenCVnaloga1.py
@@ -14,7 +14,9 @@ def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) ->
     Primer: Če je v sliki 25 škatel, kjer je v vsaki vrstici 5 škatel, naj bo seznam oblike
       [[1,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1]].
       V tem primeru je v prvi škatli 1 piksel kože, v drugi 0, v tretji 0, v četrti 1 in v peti 1.'''
-    pass
+    spodnja_meja = np.array(barva_koze[0], dtype=np.uint8)
+    zgornja_meja = np.array(barva_koze[1], dtype=np.uint8)
+
 
 
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
