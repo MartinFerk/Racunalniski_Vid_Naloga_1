012af595721c616ca331489c23b75a83e685c6d3
martin <martin.ferk1@student.um.si>
2025-03-20 21:32:58 +0100

    Prvi commit

---
+++ OpenCVnaloga1.py
@@ -0,0 +1,11 @@
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
+    pass
+
+def zmanjsaj_sliko(slika,sirina,visina):
+    pass
+
+def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle,barva_koze):
+    pass
+
+def prestej_piksle_z_barvo_koze(slika, barva_koze):
+    pass
